8f53b3839f0a2d73602c9988348aa6042c92ca48
pranjal <pranjal@lumosys.com>
2026-02-10 16:35:12 +0530

    register updated

---
+++ doctor_booking/accounts/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from .models import *
+
+class RegisterSerializer(serializers.ModelSerializer):
+    role = serializers.ChoiceField(choices=Profile.ROLE_CHOICES)
+
+    class Meta:
+        model = User
+        fields = ['username','password', 'role']
+        extra_kwargs = {'password':{'write_only':True}}
+
+    def create(self, validated_data):
+        role = validated_data.pop('role')
+        user = User.objects.create_user(**validated_data)
+        Profile.objects.create(user=user,role=role)
+        return user
+      
--- doctor_booking/accounts/views.py
+++ doctor_booking/accounts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import *
 
 # Create your views here.
+class RegisterView(APIView):
+    def post(self,request):
+        serializers = RegisterSerializer(data=request.data)
+        serializers.is_valid(raise_exception=True)
+        serializers.save()
+        return Response({"message":"Registered succesfully"})
+        
